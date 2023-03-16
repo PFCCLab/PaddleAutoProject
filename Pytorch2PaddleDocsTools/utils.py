a4823acc205f32a7b8145f8438e80e2100008ed0
gouzi <530971494@qq.com>
2023-03-17 00:12:14 +0800

    add get example, add ignore, add requirements.txt

--- Pytorch2PaddleDocsTools/utils.py
+++ Pytorch2PaddleDocsTools/utils.py
@@ -1,13 +1,8 @@
-import requests
 import re
 from bs4 import BeautifulSoup
 
 
 def get_torch_func(torch_page=""):
-    if torch_page is None:
-        print("输入pytorch网址错误，请重新输入！")
-        return
-
     soup = BeautifulSoup(torch_page, 'html.parser')
 
     function_name = ''
@@ -29,12 +24,7 @@ def get_torch_func(torch_page=""):
     return function_torch
 
 
-def get_paddle_func(url="https://www.paddlepaddle.org.cn/documentation/docs/zh/api/paddle/amax_cn.html"
-                    ):
-    if 'paddlepaddle' not in url:
-        print("输入paddlepaddle网址错误，请重新输入！")
-
-    paddle_page = requests.get(url).text
+def get_paddle_func(paddle_page=""):
     soup = BeautifulSoup(paddle_page, 'html.parser')
 
     for link in soup.find_all("dt", attrs={"class": "sig sig-object py"}):
@@ -52,14 +42,39 @@ def get_paddle_func(url="https://www.paddlepaddle.org.cn/documentation/docs/zh/a
     return function_paddle
 
 
-def get_torch_example(torch_page):
+def get_torch_example(torch_page=''):
+    # 正则匹配内容
     example_content = str(re.findall(
         r'<div class="highlight-default notranslate"><div class="highlight"><pre><span></span>(.*)</pre></div>',
         torch_page, re.DOTALL)[0])
+
+    # 去除无用的标签
     example_content = example_content.replace('<span class="gp">', "").replace('<span class="n">', '').replace(
         '<span class="o">', '').replace('<span class="mi">', '').replace('<span class="p">', '').replace('</span>',
                                                                                                          '').replace(
         '<span class="go">', '')
+
+    # 替换一些符号
     example_content = example_content.replace('&gt;&gt;&gt;', '>>>')
+
+    return example_content
+
+
+def get_paddle_example(paddle_page=''):
+    # 正则匹配内容
+    example_content = str(re.findall(
+        r'<h2>代码示例<a class="headerlink" href="#daimashili" title="永久链接至标题">¶</a></h2>(.*)</pre>',
+        paddle_page, re.DOTALL)[0])
+
+    # 去除无用的标签
+    example_content = example_content.replace('<span class="nn">', "").replace('<span class="n">', '').replace(
+        '<span class="o">', '').replace('<span class="mi">', '').replace('<span class="p">', '').replace(
+        '<div class="highlight-python notranslate">\n', '').replace('<div class="highlight">\n', '').replace(
+        '<span class="kn">', '').replace('</span>', '').replace('<span class="go">', '').replace('<pre>', '').replace(
+        '<span>', '').replace('<span class="nb">', '').replace('<span class="c1">', '').replace('<span class="mf">', '')
+
+    # 去除一些奇奇怪怪的空格
+    example_content = example_content.replace('                           ', '')
+    print(example_content)
 
     return example_content
