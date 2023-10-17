668dc757402292dc4a9526b980c97d8c0e290ace
hjyp <2594297576@qq.com>
2023-10-17 17:30:44 +0800

    Add new branch happy-open-source

--- HackathonBot/bot.py
+++ HackathonBot/bot.py
@@ -3,93 +3,102 @@ import json
 import schedule
 
 import utils
-from config import config, logger
+from config import configs, common_config, logger
 
 
 def update_issue_automatically():
 
-    try:
-        issue_url = config['issue_url']
-    
-        # 1. 获取issue表格
-        response = utils.request_get_issue(issue_url)
+    for config in configs:
+        try:
+            for key in common_config:
+                if key not in config:
+                    config[key] = common_config[key]
 
-        # 从issue中提取题目列表
-        task_list = utils.process_issue(response['body'])
-
-        # 2. 根据评论更新表格
-        comment_url = issue_url + '/comments'
-        comments = utils.request_get_multi(comment_url)
-        for comment in comments:
-            utils.update_status_by_comment(task_list, comment)
-            
-
-        # 3. 根据PR更新表格
-        # - 根据提出的PR更新状态为已提交
-        # - 根据close的PR更新状态为已完成
-        repo_urls = config['repo_urls']
-
-        for repo_url in repo_urls:
-            params = {
-                "state": "open"
-            }
-            pulls = utils.request_get_multi(repo_url, params)
-            for pull in pulls:
-                if pull['created_at'] > config['start_time']:
-                    utils.update_status_by_pull(task_list, pull)
-
-            params = {
-                "state": "closed"
-            }
-            pulls = utils.request_get_multi(repo_url, params)
-            for pull in pulls:
-                if pull['created_at'] > config['start_time'] and pull['merged_at']:
-                    utils.update_status_by_pull(task_list, pull)
-
-
-        # 4. 更新榜单内容
-        updated_issue = response['body']
-        for task in task_list:
-            if task == None:
-                continue
-            num = int(task['num'].strip(' '))
-            start = updated_issue.find('| {} |'.format(num))
-            end = start + 1
-            while end < len(updated_issue) and updated_issue[end] != '\r' and updated_issue[end] != '\n':
-                end += 1
-            
-            # TODO：这里后期需要定制化表头
-            row = '| {} | {} | {} |'.format(num, task['issue'], task['status'])
-            updated_issue = f'{updated_issue[:start]}{row}{updated_issue[end:]}'
+            issue_url = config['issue_url']
         
+            # 1. 获取issue表格
+            response = utils.request_get_issue(issue_url, config=config)
+
+            # 从issue中提取题目列表
+            task_list = utils.process_issue(response['body'], config)
+
+            # 2. 根据评论更新表格
+            comment_url = issue_url + '/comments'
+            comments = utils.request_get_multi(comment_url, config=config)
+            for comment in comments:
+                utils.update_status_by_comment(task_list, comment, config)
+                
+
+            # 3. 根据PR更新表格
+            # - 根据提出的PR更新状态为已提交
+            # - 根据close的PR更新状态为已完成
+            repo_urls = config['repo_urls']
+
+            for repo_url in repo_urls:
+                params = {
+                    "state": "open"
+                }
+                pulls = utils.request_get_multi(repo_url, params, config)
+                for pull in pulls:
+                    if pull['created_at'] > config['start_time']:
+                        utils.update_status_by_pull(task_list, pull, config)
+
+                params = {
+                    "state": "closed"
+                }
+                pulls = utils.request_get_multi(repo_url, params, config)
+                for pull in pulls:
+                    if pull['created_at'] > config['start_time'] and pull['merged_at']:
+                        utils.update_status_by_pull(task_list, pull, config)
+
+
+            # 4. 更新榜单内容
+            updated_issue = response['body']
+            for task in task_list:
+                if task == None:
+                    continue
+                num = int(task['col_0'].strip(' '))
+                start = updated_issue.find('| {} |'.format(num))
+                end = start + 1
+                while end < len(updated_issue) and updated_issue[end] != '\r' and updated_issue[end] != '\n':
+                    end += 1
+                
+                col_num = config["col_num"]
+                row = "| "
+                for i in range(col_num):
+                    row += "{} | ".format(task["col_" + str(i)])
+                updated_issue = f'{updated_issue[:start]}{row}{updated_issue[end:]}'
+            
+            
+            # 5. 更新看板信息，框架计划不需要更新看板信息
+            if config["board"]:
+                board_info = utils.update_board(task_list, config)
+                start = updated_issue.find('看板信息')
+                if start == -1:
+                    updated_issue += "\n## 看板信息 \n\n #####\n"
+                    start = updated_issue.find('看板信息')
+                while updated_issue[start] != '\n':
+                    start += 1
+                end = updated_issue.find('#####', start)
+                updated_issue = updated_issue[: start + 1] + board_info + updated_issue[end:]
+
+            # 6. 处理换行符，写入日志存档
+            updated_issue = updated_issue.replace('\r', '')
+            file_name = time.strftime('%Y-%m-%dT%H-%M-%S', time.localtime())
+            with open('./logs/{}.md'.format(file_name), mode='w', encoding='utf-8') as f:
+                f.write(updated_issue)
+
+            # 7. 更新 issue
+            data = {}
+            data['body'] = updated_issue
+            data['title'] = response['title']
+
+            res = utils.request_update_issue(issue_url, json.dumps(data), config)
+            logger.info('更新issue内容返回结果: ' + str(res))
         
-        # 5. 更新看板信息，框架计划不需要更新看板信息
-        board_info = utils.update_board(task_list)
-        start = updated_issue.find('看板信息')
-        if start == -1:
-            updated_issue += "\n## 看板信息 \n\n #####\n"
-            start = updated_issue.find('看板信息')
-        while updated_issue[start] != '\n':
-            start += 1
-        end = updated_issue.find('#####', start)
-        updated_issue = updated_issue[: start + 1] + board_info + updated_issue[end:]
-
-        # 6. 处理换行符，写入日志存档
-        updated_issue = updated_issue.replace('\r', '')
-        file_name = time.strftime('%Y-%m-%dT%H-%M-%S', time.localtime())
-        with open('./logs/{}.md'.format(file_name), mode='w', encoding='utf-8') as f:
-            f.write(updated_issue)
-
-        # 7. 更新 issue
-        data = {}
-        data['body'] = updated_issue
-        data['title'] = response['title']
-
-        res = utils.request_update_issue(issue_url, json.dumps(data))
-        logger.info('更新issue内容返回结果: ' + str(res))
-
-    except Exception as e:
-        logger.exception(e)
+        except Exception as e:
+            logger.error("处理{}任务时发生错误，具体错误如下：".format(config["issue_name"]))
+            logger.exception(e)
 
     
 if __name__ == '__main__':
--- HackathonBot/config.py
+++ HackathonBot/config.py
@@ -2,11 +2,7 @@ import logging
 import os
 import json
 
-# 监控的仓库列表
-repo_urls = ['https://api.github.com/repos/PaddlePaddle/PaddleScience/pulls'
-            ]
-
-config = {
+common_config = {
     # 更新issue的token
     'issue_token': os.environ.get('ISSUE_TOKEN'),
 
@@ -18,34 +14,51 @@ config = {
         'http':  os.environ.get('HTTP_PROXY'),
         'https': os.environ.get('HTTPS_PROXY')
     },
-
-    # 开始时间，只会统计开始时间之后的PR(注意时间中的字母T和Z不能缺少)
-    'start_time' : '2023-10-16T00:28:48Z',
-
-    # issue页面 url 地址, 注意结尾不要有斜杠
-    'issue_url': 'https://api.github.com/repos/PaddlePaddle/PaddleScience/issues/571',
     
-    # 监控的仓库列表
-    'repo_urls': repo_urls,
+    # 列的个数，默认为3
+    'col_num': 3, 
 
-    # 总的任务数量
-    'task_num' : 20,
+    # 是否展示看板信息
+    'board': True,
 
-    # 忽略不处理的题号，这部分留给人工处理
-    'un_handle_tasks' : [],
-
-    # 已删除的赛题
-    'removed_tasks' : [],
-
-    # 赛道名
-    'type_names' : ["热身赛"], 
-
-    # 每个赛题所属的赛道，每个赛道是一个数组
-    'task_types' : [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]],
-
-    # 该issue相关PR的前缀，用来标识PR是否属于该issue
-    'pr_prefix' : "hydra No."
+    'comment_to_user_list': []
 }
+
+configs = [
+    {
+        # 任务名称，起标识作用
+        'issue_name': "【快乐开源】基于hydra的案例改造计划",
+
+        # 开始时间，只会统计开始时间之后的PR(注意时间中的字母T和Z不能缺少)
+        'start_time' : '2023-10-16T00:28:48Z',
+
+        # issue页面 url 地址, 注意结尾不要有斜杠
+        'issue_url': 'https://api.github.com/repos/PaddlePaddle/PaddleScience/issues/571',
+        
+        # 监控的仓库列表
+        'repo_urls': ['https://api.github.com/repos/PaddlePaddle/PaddleScience/pulls'],
+
+        # 总的任务数量
+        'task_num' : 20,
+
+        # 忽略不处理的题号，这部分留给人工处理
+        'un_handle_tasks' : [],
+
+        # 已删除的赛题
+        'removed_tasks' : [],
+
+        # 赛道名
+        'type_names' : ["热身赛"], 
+
+        # 每个赛题所属的赛道，每个赛道是一个数组
+        'task_types' : [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]],
+
+        # 该issue相关PR的前缀，用来标识PR是否属于该issue
+        'pr_prefix' : "hydra No."
+        
+    }, 
+]
+    
 
 def get_logger():
     logger = logging.getLogger('logger')
