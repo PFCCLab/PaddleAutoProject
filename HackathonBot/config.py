e262485312ea4ec47014e9e2d8c68f1365281ded
Tao Luo <luotao02@baidu.com>
2023-11-15 15:51:10 +0800

    Update config.py
    删除已经完成的科学计算题目

--- HackathonBot/config.py
+++ HackathonBot/config.py
@@ -25,40 +25,6 @@ common_config = {
 configs = [
     {
         # 任务名称，起标识作用
-        'issue_name': "【快乐开源】基于hydra的案例改造计划",
-
-        # 开始时间，只会统计开始时间之后的PR(注意时间中的字母T和Z不能缺少)
-        'start_time' : '2023-10-16T00:28:48Z',
-
-        # issue页面 url 地址, 注意结尾不要有斜杠
-        'issue_url': 'https://api.github.com/repos/PaddlePaddle/PaddleScience/issues/571',
-        
-        # 监控的仓库列表
-        'repo_urls': ['https://api.github.com/repos/PaddlePaddle/PaddleScience/pulls'],
-
-        # 总的任务数量
-        'task_num' : 20,
-
-        # 忽略不处理的题号，这部分留给人工处理
-        'un_handle_tasks' : [],
-
-        # 已删除的赛题
-        'removed_tasks' : [],
-
-        # 赛道名
-        'type_names' : ["热身赛"], 
-
-        # 每个赛题所属的赛道，每个赛道是一个数组
-        'task_types' : [['1-20']],
-
-        # 该issue相关PR的前缀，用来标识PR是否属于该issue
-        'pr_prefix' : "hydra No.",
-
-        # PR、状态等信息所在的列
-        'pr_col': 3,
-        
-    }, {
-        # 任务名称，起标识作用
         'issue_name': "【黑客松】",
 
         # 黑客松开始时间，只会统计黑客松开始时间之后的PR(注意时间中的字母T和Z不能缺少)
