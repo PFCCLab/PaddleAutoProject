555c854f3c57b7877c56b04d0d5d0fdb517ac929
hjyp <2594297576@qq.com>
2024-01-24 17:33:15 +0800

    Update task number

--- HackathonBot/config.py
+++ HackathonBot/config.py
@@ -202,10 +202,10 @@ configs = [
         'repo_urls': [],
 
         # 最大的任务ID
-        'max_task_id' : 23,
+        'max_task_id' : 24,
 
         # 忽略不处理的题号，这部分留给人工处理
-        'un_handle_tasks' : [1, 2, 3, 4, 6, 10, 11, 12], 
+        'un_handle_tasks' : [1, 2, 3, 4, 6, 10, 11, 12, 24], 
 
         # 已删除的赛题
         'removed_tasks' : [],
@@ -214,7 +214,7 @@ configs = [
         'type_names' : ["HACKATHON 6th Code Camp"], 
 
         # 每个赛题所属的赛道，每个赛道是一个数组
-        'task_types' : [['1-23']],
+        'task_types' : [['1-24']],
 
         # 该issue相关PR的前缀，用来标识PR是否属于该issue
         'pr_prefix' : "HACKATHON 6th Code Camp No.",
