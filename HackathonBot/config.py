0afbc3aa48985489f31dd2f705d9673e452a6a31
Tao Luo <luotao02@baidu.com>
2024-01-29 11:58:02 +0800

    Update config.py (#84)

--- HackathonBot/config.py
+++ HackathonBot/config.py
@@ -202,7 +202,7 @@ configs = [
         'repo_urls': [],
 
         # 最大的任务ID
-        'max_task_id' : 24,
+        'max_task_id' : 25,
 
         # 忽略不处理的题号，这部分留给人工处理
         'un_handle_tasks' : [1, 2, 3, 4, 6, 10, 11, 12, 24], 
@@ -214,7 +214,7 @@ configs = [
         'type_names' : ["HACKATHON 6th Code Camp"], 
 
         # 每个赛题所属的赛道，每个赛道是一个数组
-        'task_types' : [['1-24']],
+        'task_types' : [['1-25']],
 
         # 该issue相关PR的前缀，用来标识PR是否属于该issue
         'pr_prefix' : "HACKATHON 6th Code Camp No.",
