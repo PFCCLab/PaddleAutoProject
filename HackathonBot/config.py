f7a1fb2981cc5f94c0d717d1d5101c2bb6480bf1
co63oc <co63oc@users.noreply.github.com>
2024-04-08 17:07:20 +0800

    Update config.py (#99)
    Update Fundable Projects No.3 max_task_id

--- HackathonBot/config.py
+++ HackathonBot/config.py
@@ -528,7 +528,7 @@ configs = [
         'repo_urls': ['https://api.github.com/repos/PaddlePaddle/Paddle/pulls'],
 
         # 最大的任务ID
-        'max_task_id' : 6,
+        'max_task_id' : 397,
 
         # 忽略不处理的题号，这部分留给人工处理
         'un_handle_tasks' : [],
@@ -540,7 +540,7 @@ configs = [
         'type_names' : ["Fundable Projects No.3"],
 
         # 每个赛题所属的赛道，每个赛道是一个数组
-        'task_types' : [['1-6']],
+        'task_types' : [['1-397']],
 
         # 该issue相关PR的前缀，用来标识PR是否属于该issue
         'pr_prefix' : "Hackathon 6th Fundable Projects 3 No.",
