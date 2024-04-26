47cbb0186e2ffcfc0c5d432399ac53e73f75ecb6
YuanRisheng <yuanrisheng@baidu.com>
2024-04-26 06:59:27 +0000

    add task

--- HackathonBot/config.py
+++ HackathonBot/config.py
@@ -557,6 +557,42 @@ configs = [
 
         # 是否展示看板信息
         'board': True,
+    },{
+        # 任务名称，起标识作用
+        'issue_name': "【快乐开源】PIR模式下单测问题修复与适配",
+
+        # 开始时间，只会统计开始时间之后的PR(注意时间中的字母T和Z不能缺少)
+        'start_time' : '2024-04-26T00:00:00Z',
+
+        # issue页面 url 地址, 注意结尾不要有斜杠
+        'issue_url': 'https://api.github.com/repos/PaddlePaddle/Paddle/issues/63740',
+
+        # 监控的仓库列表
+        'repo_urls': ['https://api.github.com/repos/PaddlePaddle/Paddle/pulls','https://api.github.com/repos/PaddlePaddle/docs/pulls'],
+
+        # 最大的任务ID
+        'max_task_id' : 664,
+
+        # 忽略不处理的题号，这部分留给人工处理
+        'un_handle_tasks' : [],
+
+        # 已删除的赛题
+        'removed_tasks' : [],
+
+        # 赛道名
+        'type_names' : ["【快乐开源】PIR模式下单测问题修复与适配"],
+
+        # 每个赛题所属的赛道，每个赛道是一个数组
+        'task_types' : [['1-664']],
+
+        # 该issue相关PR的前缀，用来标识PR是否属于该issue
+        'pr_prefix' : "Fix PIR Unittest",
+
+        # PR、状态等信息所在的列
+        'pr_col': 5,
+
+        # 是否展示看板信息
+        'board': True,
     }
 ]
 
