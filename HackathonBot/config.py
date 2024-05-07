c7de5607f209e5c6c9a25cd686d6de3dd13a6f39
Tao Luo <luotao02@baidu.com>
2024-05-07 10:36:44 +0800

    rm [Hackathon 6th Code Camp] (#116)

--- HackathonBot/config.py
+++ HackathonBot/config.py
@@ -307,42 +307,6 @@ configs = [
         'board': True,
     },{
         # 任务名称，起标识作用
-        'issue_name': "【Hackathon 6th Code Camp】飞桨护航计划集训营（第二批）",
-
-        # 开始时间，只会统计开始时间之后的PR(注意时间中的字母T和Z不能缺少)
-        'start_time' : '2024-03-21T00:00:00Z',
-
-        # issue页面 url 地址, 注意结尾不要有斜杠
-        'issue_url': 'https://api.github.com/repos/PaddlePaddle/Paddle/issues/62906',
-
-        # 监控的仓库列表
-        'repo_urls':  [],
-
-        # 最大的任务ID
-        'max_task_id' : 4,
-
-        # 忽略不处理的题号，这部分留给人工处理
-        'un_handle_tasks' : [],
-
-        # 已删除的赛题
-        'removed_tasks' : [],
-
-        # 赛道名
-        'type_names' : ["飞桨护航计划集训营（第二批）"],
-
-        # 每个赛题所属的赛道，每个赛道是一个数组
-        'task_types' : [['1-4']],
-
-        # 该issue相关PR的前缀，用来标识PR是否属于该issue
-        'pr_prefix' : "",
-
-        # PR、状态等信息所在的列
-        'pr_col': 5,
-
-        # 是否展示看板信息
-        'board': False,
-    },{
-        # 任务名称，起标识作用
         'issue_name': "【Hackathon 6th】优秀稿件征集与传播",
 
         # 开始时间，只会统计开始时间之后的PR(注意时间中的字母T和Z不能缺少)
@@ -590,7 +554,7 @@ configs = [
         'pr_prefix' : "Fix PIR Unittest No.",
 
         # PR、状态等信息所在的列
-        'pr_col': 5,
+        'pr_col': 4,
 
         # 是否展示看板信息
         'board': True,
