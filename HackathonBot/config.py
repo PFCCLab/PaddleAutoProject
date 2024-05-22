5b1daccf168a03d51209579d056e9c9e1c493847
risemeup1 <62429225+risemeup1@users.noreply.github.com>
2024-05-22 15:12:03 +0800

    update config.py (#122)
    * update config.py
    
    * Update config.py
    
    ---------
    
    Co-authored-by: Tao Luo <luotao02@baidu.com>

--- HackathonBot/config.py
+++ HackathonBot/config.py
@@ -489,6 +489,42 @@ configs = [
 
         # 是否展示看板信息
         'board': True,
+    },{
+        # 任务名称，起标识作用
+        'issue_name': "【快乐开源】PIR下Sparse算子Python API 适配",
+
+        # 开始时间，只会统计开始时间之后的PR(注意时间中的字母T和Z不能缺少)
+        'start_time' : '2024-05-22T00:00:00Z',
+
+        # issue页面 url 地址, 注意结尾不要有斜杠
+        'issue_url': 'https://api.github.com/repos/PaddlePaddle/Paddle/issues/64492',
+
+        # 监控的仓库列表
+        'repo_urls': ['https://api.github.com/repos/PaddlePaddle/Paddle/pulls'],
+
+        # 最大的任务ID
+        'max_task_id' : 14,
+
+        # 忽略不处理的题号，这部分留给人工处理
+        'un_handle_tasks' : [],
+
+        # 已删除的赛题
+        'removed_tasks' : [],
+
+        # 赛道名
+        'type_names' : ["【快乐开源】PIR下Sparse算子Python API 适配"],
+
+        # 每个赛题所属的赛道，每个赛道是一个数组
+        'task_types' : [['1-14']],
+
+        # 该issue相关PR的前缀，用来标识PR是否属于该issue
+        'pr_prefix' : "Migrate Sparse API No.",
+
+        # PR、状态等信息所在的列
+        'pr_col': 4,
+
+        # 是否展示看板信息
+        'board': True,
     }
 ]
 
