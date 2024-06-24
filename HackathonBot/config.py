b9ca8d859daa96605b7210e40a909f11716af6f2
You Jiangbin <30883834+Harryoung@users.noreply.github.com>
2024-06-24 10:04:39 +0800

    Update config.py (#128)
    * Update config.py
    
    add starter camp 3th season
    
    * Update config.py
    
    ---------
    
    Co-authored-by: Tao Luo <luotao02@baidu.com>

--- HackathonBot/config.py
+++ HackathonBot/config.py
@@ -605,7 +605,7 @@ configs = [
         'start_time' : '2024-06-19T00:00:00Z',
 
         # issue页面 url 地址, 注意结尾不要有斜杠
-        'issue_url': 'https://github.com/PaddlePaddle/Paddle/issues/65285',
+        'issue_url': 'https://api.github.com/repos/PaddlePaddle/Paddle/issues/65285',
 
         # 监控的仓库列表
         'repo_urls':  [],
@@ -632,6 +632,41 @@ configs = [
         'pr_col': 5,
 
         # 是否展示看板信息
+        'board': False,
+    }, {
+        # 任务名称，起标识作用
+        'issue_name': "【HACKATHON 预备营】飞桨启航计划集训营（第三期）",
+
+        # 开始时间，只会统计开始时间之后的PR(注意时间中的字母T和Z不能缺少)
+        'start_time' : '2024-06-19T00:00:00Z',
+
+        # issue页面 url 地址, 注意结尾不要有斜杠
+        'issue_url': 'https://api.github.com/repos/PaddlePaddle/Paddle/issues/65279',
+
+        # 监控的仓库列表
+        'repo_urls': [],
+
+        # 最大的任务ID
+        'max_task_id' : 5,
+
+        # 忽略不处理的题号，这部分留给人工处理
+        'un_handle_tasks' : [],
+
+        # 已删除的赛题
+        'removed_tasks' : [],
+
+        # 赛道名
+        'type_names' : ["Starter Code Camp"],
+
+        # 每个赛题所属的赛道，每个赛道是一个数组
+        'task_types' : [['1-5']],
+
+        # 该issue相关PR的前缀，用来标识PR是否属于该issue
+        'pr_prefix' : "Starter Code Camp",
+
+        # PR、状态等信息所在的列
+        'pr_col': 4,
+
         'board': False,
     }
 ]
