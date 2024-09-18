fd6fc8114a62f64d546509549960b5b8eee3a154
Tao Luo <luotao02@baidu.com>
2024-09-18 11:24:04 +0800

    Update config.py for Hackathon 7th

--- HackathonBot/config.py
+++ HackathonBot/config.py
@@ -160,42 +160,6 @@ configs = [
         'board': True,
     },{
         # 任务名称，起标识作用
-        'issue_name': "【Hackathon 6th Fundable Projects No.4】Paddle 框架旧执行器功能退场 Tracking Issue",
-
-        # 开始时间，只会统计开始时间之后的PR(注意时间中的字母T和Z不能缺少)
-        'start_time' : '2024-03-30T00:00:00Z',
-
-        # issue页面 url 地址, 注意结尾不要有斜杠
-        'issue_url': 'https://api.github.com/repos/PaddlePaddle/Paddle/issues/63510',
-
-        # 监控的仓库列表
-        'repo_urls': ['https://api.github.com/repos/PaddlePaddle/Paddle/pulls','https://api.github.com/repos/PaddlePaddle/docs/pulls'],
-
-        # 最大的任务ID
-        'max_task_id' : 6,
-
-        # 忽略不处理的题号，这部分留给人工处理
-        'un_handle_tasks' : [],
-
-        # 已删除的赛题
-        'removed_tasks' : [],
-
-        # 赛道名
-        'type_names' : ["Fundable Projects No.4"],
-
-        # 每个赛题所属的赛道，每个赛道是一个数组
-        'task_types' : [['1-6']],
-
-        # 该issue相关PR的前缀，用来标识PR是否属于该issue
-        'pr_prefix' : "Hackathon 6th Fundable Projects 4 No.",
-
-        # PR、状态等信息所在的列
-        'pr_col': 3,
-
-        # 是否展示看板信息
-        'board': True,
-    },{
-        # 任务名称，起标识作用
         'issue_name': "引入 clang-tidy Tracking Issue",
 
         # 开始时间，只会统计开始时间之后的PR(注意时间中的字母T和Z不能缺少)
@@ -261,7 +225,7 @@ configs = [
         'task_types' : [['1-17']],
 
         # 该issue相关PR的前缀，用来标识PR是否属于该issue
-        'pr_prefix' : "Hackathon 7th No.",
+        'pr_prefix' : "Hackathon 7th PPSCI No.",
 
         # PR、状态等信息所在的列
         'pr_col': 4,
