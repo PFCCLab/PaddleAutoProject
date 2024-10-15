052bbf159e015da604da18b7fa036686cf722a6b
zachary sun <sunzhongkai@baidu.com>
2024-10-15 19:23:38 +0800

    update

--- HackathonBot/config.py
+++ HackathonBot/config.py
@@ -300,7 +300,7 @@ configs = [
         'repo_urls':  [],
 
         # 最大的任务ID
-        'max_task_id' : 4,
+        'max_task_id' : 6,
 
         # 忽略不处理的题号，这部分留给人工处理
         'un_handle_tasks' : [],
