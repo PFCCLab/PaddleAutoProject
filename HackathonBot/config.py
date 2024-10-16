4d43206abe452dc3a803eefd254cd74d0c6196bf
zachary sun <70642955+sunzhongkai588@users.noreply.github.com>
2024-10-16 10:08:28 +0800

    update_hackathon (#145)
    * update
    
    * Update config.py
    
    ---------
    
    Co-authored-by: Tao Luo <luotao02@baidu.com>

--- HackathonBot/config.py
+++ HackathonBot/config.py
@@ -300,7 +300,7 @@ configs = [
         'repo_urls':  [],
 
         # 最大的任务ID
-        'max_task_id' : 4,
+        'max_task_id' : 6,
 
         # 忽略不处理的题号，这部分留给人工处理
         'un_handle_tasks' : [],
@@ -312,7 +312,7 @@ configs = [
         'type_names' : ["Fundable Projects"],
 
         # 每个赛题所属的赛道，每个赛道是一个数组
-        'task_types' : [['1-4']],
+        'task_types' : [['1-6']],
 
         # 该issue相关PR的前缀，用来标识PR是否属于该issue
         'pr_prefix' : "Hackathon 7th Fundable Projects No.",
