7c8f76a239582ea5ee4a984918bc80685f3c0916
luyao-cv <1367355728@qq.com>
2024-12-02 19:08:17 +0800

    Update config.py (#161)
    paddlemix 新增题目到ID 46

--- HackathonBot/config.py
+++ HackathonBot/config.py
@@ -302,7 +302,7 @@ configs = [
         'repo_urls': ['https://api.github.com/repos/PaddlePaddle/PaddleMIX/pulls'],
 
         # 最大的任务ID
-        'max_task_id' : 41,
+        'max_task_id' : 46,
 
         # 忽略不处理的题号，这部分留给人工处理
         'un_handle_tasks' : [],
@@ -314,7 +314,7 @@ configs = [
         'type_names' : ["飞桨多模态大模型快乐开源活动"],
 
         # 每个赛题所属的赛道，每个赛道是一个数组
-        'task_types' : [['1-41']],
+        'task_types' : [['1-46']],
 
         # 该issue相关PR的前缀，用来标识PR是否属于该issue
         'pr_prefix' : "PPMix No",
