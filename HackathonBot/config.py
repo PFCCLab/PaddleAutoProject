5522660555812c14b2060211e9eb80be879c2a8f
Tao Luo <luotao02@baidu.com>
2025-02-27 20:13:24 +0800

    Update config.py

--- HackathonBot/config.py
+++ HackathonBot/config.py
@@ -200,7 +200,7 @@ configs = [
         'type_names' : ["飞桨大语言模型快乐开源活动"],
 
         # 每个赛题所属的赛道，每个赛道是一个数组
-        'task_types' : [['1-4'],['5-35']],
+        'task_types' : [['1-35']],
 
         # 该issue相关PR的前缀，用来标识PR是否属于该issue
         'pr_prefix' : "PaddleNLP No.",
