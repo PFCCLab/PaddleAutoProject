09633a581d3b964151b770475361c52da271a40b
Yuqiang Ge <143453447+YqGe585@users.noreply.github.com>
2025-09-12 12:11:45 +0800

    增加CUDA Kernel规范化任务 (#234)
    * kernel
    
    * rename prefix

--- HackathonBot/config.py
+++ HackathonBot/config.py
@@ -265,6 +265,45 @@ configs = [
 
         # 是否为黑客松任务
         'hackathon': False,
+    },{
+        # 任务名称，起标识作用
+        'issue_name': "【启航计划】PaddlePaddle PHI算子库CUDA Kernel规范化",
+
+        # 开始时间，只会统计开始时间之后的PR(注意时间中的字母T和Z不能缺少)
+        'start_time' : '2025-09-12T00:00:00Z',
+
+        # issue页面 url 地址, 注意结尾不要有斜杠
+        'issue_url': 'https://api.github.com/repos/PaddlePaddle/Paddle/issues/75226',
+
+        # 监控的仓库列表
+        'repo_urls': ['https://api.github.com/repos/PaddlePaddle/Paddle/pulls', 'https://api.github.com/repos/PaddlePaddle/PaddleCustomDevice/pulls'],
+
+        # 最大的任务ID
+        'max_task_id' : 136,
+
+        # 忽略不处理的题号，这部分留给人工处理
+        'un_handle_tasks' : [],
+
+        # 已删除的赛题
+        'removed_tasks' : [],
+
+        # 赛道名
+        'type_names' : ["CUDA Kernel规范化"],
+
+        # 每个赛题所属的赛道，每个赛道是一个数组
+        'task_types' : [['1-136']],
+
+        # 该issue相关PR的前缀，用来标识PR是否属于该issue
+        'pr_prefix' : "CUDA Kernel No.",
+
+        # PR、状态等信息所在的列
+        'pr_col': 3,
+
+        # 是否展示看板信息
+        'board': True,
+
+        # 是否为黑客松任务
+        'hackathon': False,
     }
 ]
 
