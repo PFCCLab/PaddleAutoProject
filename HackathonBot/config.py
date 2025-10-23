2088a9a60cb74f71f21dbc49920df911e660dd13
Tao Luo <luotao02@baidu.com>
2025-10-23 21:09:30 +0800

    Update config.py

--- HackathonBot/config.py
+++ HackathonBot/config.py
@@ -81,7 +81,7 @@ configs = [
                       'https://api.github.com/repos/PaddlePaddle/PaddleScience/pulls'],
 
         # 最大的任务ID
-        'max_task_id' : 128,
+        'max_task_id' : 127,
 
         # 忽略不处理的题号，这部分留给人工处理
         'un_handle_tasks' : [],
@@ -93,7 +93,7 @@ configs = [
         'type_names' : ["框架开发任务", "FastDeploy 开发任务", "GraphNet 编译机床任务", "科学计算任务"],
 
         # 每个赛题所属的赛道，每个赛道是一个数组
-        'task_types' : [['1-19','109'], ['20-96'], ['97-102','110-128'], ['103-108']],
+        'task_types' : [['1-19','109'], ['20-96'], ['97-102','110-127'], ['103-108']],
 
         # 该issue相关PR的前缀，用来标识PR是否属于该issue
         'pr_prefix' : "Hackathon 9th No.",
@@ -142,45 +142,6 @@ configs = [
 
         # PR、状态等信息所在的列
         'pr_col': 4,
-
-        # 是否展示看板信息
-        'board': False,
-
-        # 是否为黑客松任务
-        'hackathon': True,
-    },{
-        # 任务名称，起标识作用
-        'issue_name': "【HACKATHON 9th Code Camp】黑客松护航计划集训营（正式批）",
-
-        # 开始时间，只会统计开始时间之后的PR(注意时间中的字母T和Z不能缺少)
-        'start_time' : '2025-08-20T00:00:00Z',
-
-        # issue页面 url 地址, 注意结尾不要有斜杠
-        'issue_url': 'https://api.github.com/repos/PaddlePaddle/Paddle/issues/74775',
-
-        # 监控的仓库列表
-        'repo_urls': [],
-
-        # 最大的任务ID
-        'max_task_id' : 1,
-
-        # 忽略不处理的题号，这部分留给人工处理
-        'un_handle_tasks' : [],
-
-        # 已删除的赛题
-        'removed_tasks' : [],
-
-        # 赛道名
-        'type_names' : ["护航计划集训营（正式批）"],
-
-        # 每个赛题所属的赛道，每个赛道是一个数组
-        'task_types' : [['1']],
-
-        # 该issue相关PR的前缀，用来标识PR是否属于该issue
-        'pr_prefix' : "",
-
-        # PR、状态等信息所在的列
-        'pr_col': 5,
 
         # 是否展示看板信息
         'board': False,
