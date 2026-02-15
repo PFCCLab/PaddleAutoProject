14145058a149718ccb1676c11918563e61b0b7d1
盈盈秋水 <13458973510@163.com>
2026-02-15 17:15:33 +0800

    Add two configs (#258)
    * add two configs
    
    * update bot config
    
    * Update config.py
    
    ---------
    
    Co-authored-by: Tao Luo <luotao02@baidu.com>

--- HackathonBot/config.py
+++ HackathonBot/config.py
@@ -305,6 +305,46 @@ configs = [
 
         # 是否为黑客松任务
         'hackathon': False,
+    },{
+        # 任务名称，起标识作用
+        'issue_name': "【快乐开源】PaddlePaddle 文档链接修复任务",
+
+        # 开始时间，只会统计开始时间之后的PR(注意时间中的字母T和Z不能缺少)
+        'start_time' : '2026-01-26T00:00:00Z',
+
+        # issue页面 url 地址, 注意结尾不要有斜杠
+        'issue_url': 'https://api.github.com/repos/PaddlePaddle/docs/issues/7735',
+
+        # 监控的仓库列表
+        'repo_urls': ['https://api.github.com/repos/PaddlePaddle/docs/pulls',
+                      'https://api.github.com/repos/PaddlePaddle/Paddle/pulls'],
+
+        # 最大的任务ID
+        'max_task_id' : 126,
+
+        # 忽略不处理的题号，这部分留给人工处理
+        'un_handle_tasks' : [],
+
+        # 已删除的赛题
+        'removed_tasks' : [],
+
+        # 赛道名
+        'type_names' : ["赛道一", "赛道二"],
+
+        # 每个赛题所属的赛道，每个赛道是一个数组
+        'task_types' : [['1-74'],['75-126']],
+
+        # 该issue相关PR的前缀，用来标识PR是否属于该issue
+        'pr_prefix' : "Doc Link Fix No",
+
+        # PR、状态等信息所在的列
+        'pr_col': 5,
+
+        # 是否展示看板信息
+        'board': True,
+
+        # 是否为黑客松任务
+        'hackathon': False,
     }
 ]
 
